64a5bf1a09f8c251a8fbe7a2fd85146df866cf02
Ana <anaforero91@gmail.com>
2025-01-07 20:54:29 -0500

    Punto 1 resuelto

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
 /** */
+// 1.Suma de dos números
+let num1 = Number(prompt("Escriba un número: "))
+let num2 = Number(prompt("Escriba un número: "))
+
+function suma(num1, num2){
+    result = num1 + num2;
+    return "El resultado de la suma es = " + result;
+}
+
+document.getElementById("resultado").innerText = suma(num1, num2);
